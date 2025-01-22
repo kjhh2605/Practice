b4a7066e12d41a71b507572f46d891fff4f67916
김준호 <2171168@hansung.ac.kr>
2025-01-22 19:59:36 +0900

    [Bronze V] Title: A/B, Time: 172 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int op1 = sc.nextInt();
+        int op2 = sc.nextInt();
+        double answer = (double)op1 / (double)op2;
+        System.out.println(answer);
+
+    }
+}
