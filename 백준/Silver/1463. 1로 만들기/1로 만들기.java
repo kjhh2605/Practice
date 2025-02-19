03a4cc793af047981bd20239004ca1cbc00535e1
김준호 <2171168@hansung.ac.kr>
2025-02-19 19:27:13 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18120 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -8,42 +8,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        if(n==1){
-            System.out.println(0);
-            return;
-        }
-        if(n==2 || n==3){
-            System.out.println(1);
-            return;
-        }
-        int[] dp = new int[n+1];
+        int[] dp = new int[Math.max(4,n+1)];
         dp[1] = 0;
         dp[2] = 1;
         dp[3] = 1;
         int i = 4;
         while(i <= n){
-            int a = Integer.MAX_VALUE;
-            int b = Integer.MAX_VALUE;
-            int c = Integer.MAX_VALUE;
-            int d = dp[i-1]+1;
-            if(i%2==0) {
-                a = dp[i/2] + 1;
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
-            if(i%3==0) {
-                b = dp[i/3] + 1;
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
-            if((i-1)%2==0 || (i-1)%3==0){
-                int x = Integer.MAX_VALUE;
-                int y = Integer.MAX_VALUE;
-                if((i-1)%2==0){
-                    x=dp[(i-1)/2]+2;
-                }
-                if((i-1)%3==0){
-                    y=dp[(i-1)/3]+2;
-                }
-                c=Math.min(x,y);
-            }
-            dp[i]=Math.min(Math.min(a,b),Math.min(c,d));
             i++;
         }
         System.out.println(dp[n]);
