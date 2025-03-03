ceaba0d7fb751af415a27bb4037250687d50a187
김준호 <2171168@hansung.ac.kr>
2025-03-03 14:28:23 +0900

    [Gold V] Title: 적록색약, Time: 152 ms, Memory: 18236 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -2,101 +2,71 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        char[][] grid = new char[n][n];
-        ArrayDeque<int[]> q = new ArrayDeque();
-        ArrayDeque<int[]> qq = new ArrayDeque();
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < s.length(); j++) {
-                grid[i][j] = s.charAt(j);
-                q.add(new int[]{i, j});
-                qq.add(new int[]{i, j});
+    public static int[] dx = {0, 0, -1, 1};
+    public static int[] dy = {-1, 1, 0, 0};
+    public static boolean[][] checked;
 
-            }
-        }
-
-        boolean[][] checked = new boolean[n][n];
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {-1, 1, 0, 0};
+    public static int bfs(char[][] grid, ArrayDeque<int[]> graph, int n) {
+        checked = new boolean[n][n];  // 전역 변수 재사용
         int cnt = 0;
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
+
+        while (!graph.isEmpty()) {
+            int[] cur = graph.poll();
             int x = cur[0];
             int y = cur[1];
 
-            if (checked[x][y])
-                continue;
-            else
-                checked[x][y] = true;
+            if (checked[x][y]) continue;
 
             ArrayDeque<int[]> search = new ArrayDeque<>();
             search.add(cur);
             char c = grid[x][y];
+            checked[x][y] = true;
+
             while (!search.isEmpty()) {
                 int[] tmp = search.poll();
                 int curX = tmp[0];
                 int curY = tmp[1];
+
                 for (int i = 0; i < 4; i++) {
-                    if (curX + dx[i] < 0 || curY + dy[i] < 0 || curX + dx[i] >= n || curY + dy[i] >= n)
+                    int nx = curX + dx[i];
+                    int ny = curY + dy[i];
+
+                    if (nx < 0 || ny < 0 || nx >= n || ny >= n || checked[nx][ny])
                         continue;
-                    if (c == 'B') {
-                        if (checked[curX + dx[i]][curY + dy[i]] || grid[curX + dx[i]][curY + dy[i]] != c)
-                            continue;
-                        else {
-                            search.add(new int[]{curX + dx[i], curY + dy[i]});
-                            checked[curX + dx[i]][curY + dy[i]] = true;
-                        }
-                    } else {
-                        if (checked[curX + dx[i]][curY + dy[i]] || grid[curX + dx[i]][curY + dy[i]] == 'B')
-                            continue;
-                        else {
-                            search.add(new int[]{curX + dx[i], curY + dy[i]});
-                            checked[curX + dx[i]][curY + dy[i]] = true;
-                        }
-                    }
+                    if (grid[nx][ny] != c) continue;
+
+                    search.add(new int[]{nx, ny});
+                    checked[nx][ny] = true;
                 }
             }
             cnt++;
         }
+        return cnt;
+    }
 
-        boolean checked1[][] = new boolean[n][n];
-        int count = 0;
-        while (!qq.isEmpty()) {
-            int[] cur = qq.poll();
-            int x = cur[0];
-            int y = cur[1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        char[][] grid = new char[n][n];
+        char[][] grid2 = new char[n][n];
+        ArrayDeque<int[]> q1 = new ArrayDeque<>();
+        ArrayDeque<int[]> q2 = new ArrayDeque<>();
 
-            if (checked1[x][y])
-                continue;
-            else
-                checked1[x][y] = true;
-
-            ArrayDeque<int[]> search = new ArrayDeque<>();
-            search.add(cur);
-            char c = grid[x][y];
-            while (!search.isEmpty()) {
-                int[] tmp = search.poll();
-                int curX = tmp[0];
-                int curY = tmp[1];
-                for (int i = 0; i < 4; i++) {
-                    if (curX + dx[i] < 0 || curY + dy[i] < 0 || curX + dx[i] >= n || curY + dy[i] >= n)
-                        continue;
-                    if (checked1[curX + dx[i]][curY + dy[i]] || grid[curX + dx[i]][curY + dy[i]] != c)
-                        continue;
-                    else {
-                        search.add(new int[]{curX + dx[i], curY + dy[i]});
-                        checked1[curX + dx[i]][curY + dy[i]] = true;
-                    }
-                }
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < n; j++) {
+                char c = s.charAt(j);
+                grid[i][j] = c;
+                if (c == 'G')
+                    grid2[i][j] = 'R';
+                else
+                    grid2[i][j] = c;
+                q1.add(new int[]{i, j});
+                q2.add(new int[]{i, j});
             }
-            count++;
         }
-        System.out.println(count+" "+cnt);
+        System.out.println(bfs(grid, q1, n) + " " + bfs(grid2, q2, n));
     }
 }
