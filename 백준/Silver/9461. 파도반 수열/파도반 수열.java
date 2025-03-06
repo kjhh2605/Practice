45ebd4d68387e36a4bfa36fc4ffbda5bed604e07
김준호 <2171168@hansung.ac.kr>
2025-03-06 22:31:27 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -14,7 +14,11 @@ public class Main {
         dp[1] = 1;
         dp[2] = 1;
         dp[3] = 1;
-        for(int i = 4 ; i <= 100 ; i++){
+        dp[4] = 2;
+        dp[5] = 2;
+        dp[7] = 4;
+        dp[6] = 3;
+        for(int i = 6 ; i <= 100 ; i++){
             dp[i] = dp[i-2] + dp[i-3];
         }
         while(T-- > 0){
